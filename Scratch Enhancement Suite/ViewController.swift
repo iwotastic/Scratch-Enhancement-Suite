e6d28c993fa7c6246a998257d4fb29149a25a551
Ian Morrill <ianmorrill@icloud.com>
2016-12-22 14:10:02 -0500

    Initial Commit

---
+++ Scratch Enhancement Suite/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scratch Enhancement Suite
+//
+//  Created by Ian Morrill on 12/22/16.
+//  Copyright © 2016 Ian Morrill. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
