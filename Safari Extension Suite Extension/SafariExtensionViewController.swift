c112db09cf564506a925673c963ab47c497f2b63
Ian Morrill <ianmorrill@ians-mbp.home>
2016-12-26 09:06:48 -0500

    Make sure upgrade don't break it.

---
+++ Safari Extension Suite Extension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  Safari Extension Suite Extension
+//
+//  Created by Ian Morrill on 12/22/16.
+//  Copyright © 2016 Ian Morrill. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
